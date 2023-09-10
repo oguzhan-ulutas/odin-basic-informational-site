801c27862c7af068ceb08dcdece14d1f748793bf
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-09-10 19:09:27 +0300

    Finish using static folder

---
+++ indexWithExpress.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+const app = express();
+const port = 8080;
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '01-index.html'));
+// });
+
+// app.get('/about', (req, res) => {
+//   res.sendFile(path.join(__dirname, '02-about.html'));
+// });
+
+// app.get('/contact-me', (req, res) => {
+//   res.sendFile(path.join(__dirname, '03-contact-me.html'));
+// });
+
+// app.all('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '04-404.html'));
+// });
+
+// Setting a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {});
